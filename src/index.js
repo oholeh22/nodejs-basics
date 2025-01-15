b8236321b5146eb3077dea918b361ea8d2ad2391
Oleh Shyian <shiyan4242@gmail.com>
2025-01-15 17:04:19 +0100

    server

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { startServer } from './server.js';
+
+startServer();
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
