9821bef74fda447476287a91abb8597492820cd6
Oleh Shyian <shiyan4242@gmail.com>
2025-01-17 14:55:21 +0100

    mb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
+import { getAllStudents, getStudentById } from './services/students.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -43,3 +43,29 @@ export const startServer = () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
+
+app.get('/students', async (req, res) => {
+  const students = await getAllStudents();
+
+  res.status(200).json({
+    data: students,
+  });
+});
+
+app.get('/students/:studentId', async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  // Відповідь, якщо контакт не знайдено
+if (!student) {
+  res.status(404).json({
+    message: 'Student not found'
+  });
+  return;
+}
+
+// Відповідь, якщо контакт знайдено
+  res.status(200).json({
+    data: student,
+  });
+});
---
+++ src/services/students.js
@@ -0,0 +1,12 @@
+
+import { StudentsCollection } from '../db/models/student.js';
+
+export const getAllStudents = async () => {
+  const students = await StudentsCollection.find();
+  return students;
+};
+
+export const getStudentById = async (studentId) => {
+  const student = await StudentsCollection.findById(studentId);
+  return student;
+};
