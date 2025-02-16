667474e2d093b6037db729906059707e164a0dbf
Oleh Shyian <shiyan4242@gmail.com>
2025-02-16 09:02:29 +0100

    6.2

--- src/routers/students.js
+++ src/routers/students.js
@@ -12,22 +12,23 @@ import {
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createStudentSchema, updateStudentSchema } from '../validation/students.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/students.js';
+import { authenticate } from '../middlewares/authenticate.js';
+import { upload } from '../middlewares/multer.js';
 
 const router = Router();
 
-router.get('/students', ctrlWrapper(getStudentsController));
+router.get('/', ctrlWrapper(getStudentsController));
 
-router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
-
-router.post('/students', ctrlWrapper(createStudentController));
-
-router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-
-router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
-
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.get(
+  '/:studentId',
+  isValidId,
+  ctrlWrapper(getStudentByIdController)
+);
 
 router.post(
   '/',
@@ -35,22 +36,95 @@ router.post(
   ctrlWrapper(createStudentController),
 );
 
+router.delete(
+  '/:studentId',
+  isValidId,
+  ctrlWrapper(deleteStudentController));
+
 router.put(
-  '/students/:studentId',
+  '/:studentId',
+  isValidId, 
   validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 
 router.patch(
-  '/students/:studentId',
+  '/:studentId',
+  isValidId,
   validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
 
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getStudentsController));
+
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
+
+router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getStudentsController));
+
 router.get(
   '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
   ctrlWrapper(getStudentByIdController),
+);
+
+router.post(
+  '/',
+  checkRoles(ROLES.TEACHER),
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
+router.put(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
+
+router.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  ctrlWrapper(deleteStudentController),
+);
+
+router.post(
+  '/',
+  checkRoles(ROLES.TEACHER),
+  upload.single('photo'), // додаємо цю middleware
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
+router.put(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  upload.single('photo'), // додаємо цю middleware
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  upload.single('photo'), // додаємо цю middleware
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
 );
 
 export default router;
